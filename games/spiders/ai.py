ae0f2cf200fbfa674d1adce7572a862fff29cc28
Brian W. Goldman <brianwgoldman@acm.org>
2016-04-23 17:50:14 -0600

    Spit and run

--- games/spiders/ai.py
+++ games/spiders/ai.py
@@ -4,6 +4,30 @@ from joueur.base_ai import BaseAI
 
 import random
 
+rps = {"Weaver" : {"Weaver", "Cutter"},
+       "Cutter" : {"Cutter", "Spitter"},
+       "Spitter" : {"Spitter", "Weaver"}}
+
+prime_rps = {"Weaver" : "Cutter",
+             "Cutter" : "Spitter",
+             "Spitter" : "Weaver"}
+
+def is_valid_web(spider, web):
+    if web.strength > web.load:
+        return True
+    mine = [on_web for on_web in web.spiderlings if on_web.owner == spider.owner]
+    if len(mine) > (len(web.spiderlings) / 2):
+        return False
+    return True
+        
+
+def is_valid_spit_connection(spider, nest):
+    for web in spider.nest.webs:
+        if web.nest_a == nest or web.nest_b == nest:
+            return False
+    return True
+
+
 class AI(BaseAI):
     """ The basic AI functions that are the same between games. """
 
@@ -38,13 +62,88 @@ class AI(BaseAI):
             reason (str): The human readable string explaining why you won or lost.
         """
 
+    def setup(self):
+        self.my_brood = [spider for spider in self.player.spiders if spider.game_object_name == "BroodMother"][0]
+        self.their_brood = [spider for spider in self.player.other_player.spiders if spider.game_object_name == "BroodMother"][0]
+        self.need_jobs = [spider for spider in self.player.spiders if spider.game_object_name != "BroodMother" and spider.busy == ""]
+    
+    def spawn(self):
+        while self.my_brood.eggs > 0:
+            spider_type = random.choice(["Cutter", "Weaver", "Spitter"])
+            print("Spawning", spider_type)
+            self.my_brood.spawn(spider_type)
 
+    def do_attack(self, spider):
+        if spider.busy != "":
+            print("Trying to attack with a busy spider")
+            return True
+        targets = [target for target in spider.nest.spiders
+                   if target.owner != spider.owner and target.game_object_name in rps[spider.game_object_name]]
+        if targets:
+            prime_targets = [target for target in targets if prime_rps[spider.game_object_name] == target.game_object_name]
+            if prime_targets:
+                targets = prime_targets
+            # TODO Preferential attack based on busy
+            target = random.choice(targets)
+            print(spider.game_object_name, "attacking", target.game_object_name)
+            spider.attack(target)
+            return True
+        return False
+    
+    def do_move(self, spider):
+        if spider.busy != "":
+            print("Trying to move with a busy spider")
+            return True
+        valid_webs = [web for web in spider.nest.webs if is_valid_web(spider, web)]
+        if len(valid_webs) == 0:
+            return False
+        # TODO Better move choice logic
+        best = random.choice(spider.nest.webs)
+        print("Moving spider")
+        spider.move(best)
+        return True
+    
+    def do_spit(self, spider):
+        if spider.busy != "":
+            print("Trying to spit with a busy spider")
+            return True
+        if spider.game_object_name != "Spitter":
+            print("Trying to spit with", spider.game_object_name)
+            return False
+        valid_nests = [nest for nest in self.game.nests if is_valid_spit_connection(spider, nest)]
+        if len(valid_nests) == 0:
+            return False
+        brood_nest = [nest for nest in valid_nests if nest == self.their_brood.nest]
+        if brood_nest:
+            valid_nests = brood_nest
+        choice = random.choice(valid_nests)
+        spider.spit(choice)
+        return True
+    
     def run_turn(self):
         """ This is called every time it is this AI.player's turn.
 
         Returns:
             bool: Represents if you want to end your turn. True means end your turn, False means to keep your turn going and re-call this function.
         """
+        # TODO Consume
+        # TODO Special actions
+        self.setup()
+        self.spawn()
+        
+        for spider in self.need_jobs:
+            if self.do_attack(spider):
+                # Attacks cause spiders to be busy, right?
+                assert(spider.busy != "")
+                continue
+            if spider.game_object_name == "Spitter":
+                if self.do_spit(spider):
+                    assert(spider.busy != "")
+                    continue
+            if self.do_move(spider):
+                assert(spider.is_dead or spider.busy != "")
+                continue
+        return True
         # This is ShellAI, it is very simple, and demonstrates how to use all
         # the game objects in Spiders.
         spider = random.choice(self.player.spiders)
