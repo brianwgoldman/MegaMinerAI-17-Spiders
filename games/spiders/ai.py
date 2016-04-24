03bf6f41e41a8dee4f44fc831f9ab53afded9314
Brian W. Goldman <brianwgoldman@acm.org>
2016-04-24 10:24:07 -0600

    Cleaned up code, should contain no logical changes

--- games/spiders/ai.py
+++ games/spiders/ai.py
@@ -3,18 +3,15 @@
 from joueur.base_ai import BaseAI
 
 import random
-import math
 
+# Given a spider type, what can they kill
 rps = {"Weaver" : {"Weaver", "Spitter"},
        "Cutter" : {"Cutter", "Weaver"},
        "Spitter" : {"Spitter", "Cutter"}}
-
+#Given a spider type, what can they kill without dying themselves
 prime_rps = {"Weaver" : "Spitter",
              "Cutter" : "Weaver",
              "Spitter" : "Cutter"}
-
-def distance(a, b):
-    return math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
 
 def is_valid_web(spider, web):
     if web.strength > web.load:
@@ -69,95 +66,52 @@ class AI(BaseAI):
         """
 
     def setup(self):
+        '''
+        Create some useful collections
+        '''
         self.my_brood = [spider for spider in self.player.spiders if spider.game_object_name == "BroodMother"][0]
         self.their_brood = [spider for spider in self.player.other_player.spiders if spider.game_object_name == "BroodMother"][0]
         self.need_jobs = [spider for spider in self.player.spiders if spider.game_object_name != "BroodMother" and spider.busy == ""]
-        self.spit_pairs = {(spider.nest.id, spider.spitting_web_to_nest) for spider in self.player.spiders
-                           if spider.game_object_name == "Spitter" and spider.busy == "Spitting"}
     
     def spawn(self):
+        '''
+        Handes spawn logic, which is basically just "fill the world with Spitters"
+        '''
         while self.my_brood.eggs > 0:
-            spider_type = random.choice(["Cutter", "Weaver", "Spitter"])
-            spider_type = "Spitter"
-            #print("Spawning", spider_type)
-            self.my_brood.spawn(spider_type)
+            self.my_brood.spawn("Spitter")
 
     def do_attack(self, spider):
+        '''
+        If there is an enemy with you that you can kill, kill it.
+        Prefers kills that don't kill you too.
+        '''
         if spider.busy != "":
             print("Trying to attack with a busy spider")
             return True
         if spider.nest == self.my_brood.nest:
-            # Never attack at your own brood nest
+            # Never attack at your own brood nest, since there can't be any enemies
             return False
+        # Find living enemies you can kill
         targets = [target for target in spider.nest.spiders
                    if target.owner != spider.owner and
                    not target.is_dead and 
                    target.game_object_name in rps[spider.game_object_name]]
         if targets:
+            # Prefer enemies that don't get you killed
             prime_targets = [target for target in targets if prime_rps[spider.game_object_name] == target.game_object_name]
             if prime_targets:
                 targets = prime_targets
-            # TODO Preferential attack based on busy
             target = random.choice(targets)
             print(spider.game_object_name, "attacking", target.game_object_name)
             spider.attack(target)
-            assert not spider.is_dead or target.is_dead, "Suicide without gain!"
             return True
         return False
     
-    def do_move(self, spider):
-        if spider.busy != "":
-            print("Trying to move with a busy spider")
-            return True
-        valid_webs = [web for web in spider.nest.webs if is_valid_web(spider, web)]
-        brood_connected = [web for web in valid_webs if web == self.their_brood.nest]
-        # TODO Better move choice logic
-        if spider.nest != self.my_brood.nest or brood_connected:
-            valid_webs = brood_connected
-        if len(valid_webs) == 0:
-            return False
-
-        best = random.choice(valid_webs)
-        print("Moving", spider.game_object_name, best.load, best.strength)
-        spider.move(best)
-        if spider.is_dead:
-            print("Died in a blaze of glory during move")
-        return True
-    
-    def do_spit(self, spider):
-        if spider.busy != "":
-            print("Trying to spit with a busy spider")
-            return True
-        if spider.game_object_name != "Spitter":
-            print("Trying to spit with", spider.game_object_name)
-            return False
-        '''
-        if len([web for web in spider.nest.webs if is_valid_web(spider, web)]) > 0:
-            print("Available outbound webs, skip spitting")
-            return False
-        '''
-        valid_nests = [nest for nest in self.game.nests if is_valid_spit_connection(spider, nest)]
-        if len(valid_nests) == 0:
-            return False
-        brood_nest = [nest for nest in valid_nests if nest == self.their_brood.nest]
-        if brood_nest:
-            valid_nests = brood_nest
-        join_nests = [nest for nest in valid_nests
-                      if (nest.id, spider.nest.id) in self.spit_pairs
-                      or (spider.nest.id, nest.id) in self.spit_pairs]
-        if join_nests:
-            print("Joining spit")
-            valid_nests = join_nests
-        if not valid_nests:
-            print("WHAT HTE HELL!")
-            return False
-        choice = min(valid_nests, key=lambda nest : distance(spider.nest, nest) + distance(nest, self.their_brood.nest))
-        print("Spitting to nest", choice.id)
-        spider.spit(choice)
-        self.spit_pairs.add((spider.nest.id, choice.id))
-        return True
-    
     def defensive_move(self, spider):
+        '''
+        Find webs connected to spider.nest that contain enemy spiders,
+        and try to break that web by overloading it
+        '''
         if spider.busy != "":
             print("Trying to defensively move a busy spider")
             return True
@@ -170,14 +124,19 @@ class AI(BaseAI):
                     need_defense.append(web)
                     break
         if need_defense:
+            # Choose the web closest to breaking to do first
             web = min(need_defense, key=lambda web: web.strength - web.load)
-            print("Incomming asshole!")
+            print("Incoming asshole!")
             spider.move(web)
             return True
         return False
 
     def expand_move(self, spider):
+        '''
+        Look for places you don't know you control, and try to control them
+        '''
         if len(spider.nest.spiders) == 1:
+            # Lone spiders have to hold the nest
             return False
         if spider.busy != "":
             print("Trying to expand move a busy spider")
@@ -185,24 +144,28 @@ class AI(BaseAI):
         valid_webs = [web for web in spider.nest.webs if is_valid_web(spider, web)]
         if not valid_webs:
             return False
-        # TODO Include "in flight" bad guys
+        # TODO Include "in flight" spiders
         empty_both = [web for web in valid_webs if len(web.spiderlings) == 0 and
                       (len(web.nest_a.spiders) == 0 or len(web.nest_b.spiders) == 0)]
         if empty_both:
             valid_webs = empty_both
         else:
+            # If there are no empty nests, try to attack their broodmother
             brood = [web for web in valid_webs if web.nest_a == self.their_brood or web.nest_b == self.their_brood]
             if brood:
                 print("BROOD WARS")
                 valid_webs = brood
+        # Prefer webs with low load
         choice = min(valid_webs, key=lambda web: len(web.spiderlings))
-        #print("Expand the motherland")
         spider.move(choice)
-        
         return True
         
 
     def spray_spit(self, spider):
+        '''
+        Make a bunch of random connections, preferentially to empty nests.
+        If there are no empty nests, connect to their broodmother
+        '''
         if spider.busy != "":
             print("Trying to spit with a busy spider")
             return True
@@ -212,6 +175,7 @@ class AI(BaseAI):
         valid_nests = [nest for nest in self.game.nests if is_valid_spit_connection(spider, nest)]
         if len(valid_nests) == 0:
             return False
+        # TODO Add "in flight" spiders
         empty_nests = [nest for nest in valid_nests if len(nest.spiders) == 0]
         if empty_nests:
             valid_nests = empty_nests
@@ -220,32 +184,26 @@ class AI(BaseAI):
             if brood and spider.nest != self.my_brood.nest:
                 print("GO AFTER THE BROOD")
                 valid_nests = brood
-        if not valid_nests:
-            print("WHAT HTE HELL!")
-            return False
         choice = random.choice(valid_nests)
-        #print("Spitting to nest", choice.id)
         spider.spit(choice)
-        self.spit_pairs.add((spider.nest.id, choice.id))
         return True
     
     
     def run_turn(self):
-        """ This is called every time it is this AI.player's turn.
-
-        Returns:
-            bool: Represents if you want to end your turn. True means end your turn, False means to keep your turn going and re-call this function.
-        """
-        # TODO Consume
-        # TODO Special actions
+        '''
+        Each non busy spider tries to
+        1. Attack enemy spiders in their nest
+        2. Overload webs that have enemy spiders on them.
+        3. Move until there is exactly 1 spider at all nests
+        4. If they are a Spitter, try to connect to nests with no spiders 
+        '''
         self.setup()
         print("Starting turn:", self.game.current_turn, "Time remaining:", self.player.time_remaining, "Score:", self.my_brood.health, self.their_brood.health)
         self.spawn()
+        # Rerun setup to find spawned things
         self.setup()
         for spider in self.need_jobs:
             if self.do_attack(spider):
-                # Attacks cause spiders to be busy, right?
-                assert (spider.is_dead or spider.busy != ""), "Attacked but not busy?"
                 continue
             if self.defensive_move(spider):
                 continue
@@ -254,88 +212,4 @@ class AI(BaseAI):
             if spider.game_object_name == "Spitter":
                 if self.spray_spit(spider):
                     continue
-            '''
-            if self.do_move(spider):
-                assert (spider.is_dead or spider.busy != ""), "Moved but not busy?"
-                continue
-            if spider.game_object_name == "Spitter":
-                if self.do_spit(spider):
-                    assert spider.busy != "", "Spit but not busy?"
-                    continue
-            '''
         return True
-        # This is ShellAI, it is very simple, and demonstrates how to use all
-        # the game objects in Spiders.
-        spider = random.choice(self.player.spiders)
-
-        if spider.game_object_name == "BroodMother":
-            brood_mother = spider
-            choice = random.randint(1, 10)
-
-            if choice == 1: # try to consume a spiderling 10% of the time
-                if len(brood_mother.nest.spiders) > 1:
-                    otherSpider = random.choice(brood_mother.nest.spiders)
-                    if otherSpider != brood_mother:
-                        print("BroodMother #" + brood_mother.id +
-                              " consuming " + otherSpider.game_object_name +
-                              " #" + otherSpider.id)
-                        brood_mother.consume(otherSpider)
-            else: # try to spawn a Spiderling
-                if brood_mother.eggs > 0:
-                    # get a random spiderling type to spawn a new
-                    # Spiderling of that type
-                    randomSpiderlingType = random.choice(["Cutter", "Weaver", "Spitter"])
-                    print("BroodMother #" + brood_mother.id +
-                          " spawning " + randomSpiderlingType)
-                    brood_mother.spawn(randomSpiderlingType)
-        else: # it is a Spiderling
-            spiderling = spider
-
-            if spiderling.busy == "": # then it is NOT busy
-                choice = random.randint(0, 2)
-
-                if choice == 0: # try to move somewhere
-                    if len(spiderling.nest.webs) > 0:
-                        web = random.choice(spiderling.nest.webs)
-                        print("Spiderling " + spiderling.game_object_name +
-                              " #" + spiderling.id + " moving on Web #" + web.id)
-                        spiderling.move(web)
-                elif choice == 1: # try to attack something
-                    if len(spiderling.nest.spiders) > 1:
-                        otherSpider = random.choice(spiderling.nest.spiders)
-                        if otherSpider.owner != spiderling.owner: # attack the enemy!
-                            spiderling.attack(otherSpider)
-                else: # do something unique based on Spiderling type
-                    if spiderling.game_object_name == "Spitter":
-                        spitter = spiderling
-                        enemysNest = self.player.other_player.brood_mother.nest
-
-                        # look for an existing web to the enemy home nest
-                        webExists = any(True
-                                        for web in enemysNest.webs
-                                        if spitter.nest in (web.nest_a, web.nest_b))
-
-                        if not webExists:
-                            print("Spitter #" + spitter.id +
-                                  " spitting to Nest #" + enemysNest.id)
-                            spitter.spit(enemysNest)
-                    elif spiderling.game_object_name == "Cutter":
-                        cutter = spiderling
-                        if len(cutter.nest.webs) > 0:
-                            web = random.choice(cutter.nest.webs)
-                            print("Cutter #" + cutter.id +
-                                  " cutting Web #" + web.id)
-                            cutter.cut(web)
-                    elif spiderling.game_object_name == "Weaver":
-                        weaver = spiderling
-                        if len(weaver.nest.webs) > 0:
-                            web = random.choice(weaver.nest.webs)
-                            if random.randint(0,1) == 1:
-                                print("Weaver #" + weaver.id +
-                                      " strengthening Web #" + web.id)
-                                weaver.strengthen(web)
-                            else:
-                                print("Weaver #" + weaver.id +
-                                      " weakening Web #" + web.id)
-                                weaver.weaken(web)
-        return True # To signify that we are done with our turn
